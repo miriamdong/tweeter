ba2c5059de49740beb9aa25bca9d88967cbd6556
miriamdong <miriamdong@gmai.com>
2021-03-23 18:43:56 +0000

    add a button to go back to the top of the page

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -34,28 +34,7 @@ $(document).ready(function() {
   //   }, 1000);
   // });
 
-  //Get the button:
-  const topButton = document.getElementById("go-to-top");
-
-  // When the user scrolls down 20px from the top of the document, show the button
-  $(window).onscroll = function() {
-    scrollFunction();
-  };
-
-  function scrollFunction() {
-    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-      topButton.style.display = "block";
-    } else {
-      topButton.style.display = "none";
-    }
-  }
-
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
-
+  
 
 
 
