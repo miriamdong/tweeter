e1d025c18fb350ceac82e5a9c7985bf261479493
miriamdong <miriamdong@gmai.com>
2021-03-23 04:14:56 +0000

    Revert "Compose Tweet box with a textarea for new tweet content"
    This reverts commit 7481cd709af05ef902a629913d2ec0faaea4612f.

--- public/scripts/composer-char-counter.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("#btn").on('click', function() {
-    console.log(this); //The this keyword is a reference to the button
-  });
-
-  $("#btn").on('click', () => {
-    console.log(this); //The this keyword here refers to something else!
-  });
-});
