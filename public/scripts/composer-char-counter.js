e13e88fe4d28de908b590d3f0cb037b163ddfc13
miriamdong <miriamdong@gmai.com>
2021-03-23 04:29:23 +0000

    Compose Tweet box with a textarea for new tweet content

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+
+
+  $("#btn").on('click', function() {
+    console.log(this); //The this keyword is a reference to the button
+  });
+
+  $("#btn").on('click', () => {
+    console.log(this); //The this keyword here refers to something else!
+  });
+});
